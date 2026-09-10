d1a3bcabb5cf59bafc10150def405ea05f1c9838
ExtremeXT <extremext@lineageos.org>
2026-09-10 13:54:10 +0000

    universal2100-common: Build NXP UWB Service
    Change-Id: If254a5a3c258d2f7893f47d1465e2dc67f5695e9

--- common.mk
+++ common.mk
@@ -362,6 +362,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.uwb.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.uwb.xml
+
 # Weaver
 PRODUCT_PACKAGES += \
     android.hardware.weaver@1.0.vendor:64
@@ -387,3 +390,4 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.aware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.aware.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
+
